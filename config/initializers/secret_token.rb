67a6f90a1532807571ba345f5fed159409f59cc3
Jaudat Syed <sjh.jaudat@gmail.com>
2013-07-01 12:51:09 -0400

    Initial Rails commit with sqlite to pg changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Honey::Application.config.secret_key_base = '04b751baeb05d16effca89a1d6e10a639fc0b0df29d36f20521ef310f8b0f685f66d412c552626aad93c2370c5c760d155c186cb73807a81fcdfc38603ff3cc5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Honey::Application.config.session_store :cookie_store, key: '_honey_session'
